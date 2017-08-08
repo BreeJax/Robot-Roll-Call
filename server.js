2b84202c5cc0da26e49c8e9d0a71be2320857d3e
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-07 22:31:07 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const pgp = require("pg-promise")()
+const mustacheExpress = require("mustache-express")
+const userDirectory = require("./data")
+
+const app = express()
+app.engine("mustache", mustacheExpress())
+
+// CREATE TABLE robots ("id" SERIAL PRIMARY KEY, "username" VARCHAR(100) NOT NULL, "imagery" VARCHAR( 300) NOT NULL, "email" VARCHAR(200) NULL,"university" VARCHAR(100) NULL, "street-number" VARCHAR(50) NULL, "address" VARCHAR(100) NULL,
+// "city" VARCHAR(100) NULL, "state" VARCHAR(100) NULL, "job" VARCHAR(100) NULL, "company" VARCHAR(100) NULL, "postal_code" INTEGER NULL, "year_built" INTEGER NULL, "next_service_date" DATE NULL, "is_active" BOOLEAN NOT NULL);
+
+app.set("views", "./templates")
+
+app.set("view engine", "mustache")
+
+app.use(express.static("public"))
+
+app.get("/info/:id", (request, responce) => {
+  const requestId = parseInt(request.params.id)
+  const foundUser = userDirectory.users.find(user => user.id === requestId)
+  responce.render("info", foundUser)
+})
+
+app.get("/", (request, responce) => {
+  responce.render("home", userDirectory)
+})
+
+app.listen(3000, () => {
+  console.log("machine is running now")
+})
